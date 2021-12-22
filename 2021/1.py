8dff50f9bcf0c125ce51a12a8ed64effb3f3df4d
Oliver <omoell@gmail.com>
2021-12-22 19:43:58 +0100

    implemented solution for puzzle 1a

--- 2021/1.py
+++ 2021/1.py
@@ -1,16 +1,32 @@
 class Puzzle_Input:
-
     def __init__(self, filename):
         self.filename = filename
+        self.list = self.read()
 
     def read(self):
         with open(self.filename, 'r') as f:
             content_raw = f.readlines()
             # Clean printed newline
-            content = [line.replace('\n','') for line in content_raw]
-        return content
+        return [line.replace('\n','') for line in content_raw]
+
+class Comparer:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def a_lgt_b(self):
+        return self.a > self.b
 
 # Puzzle 1a
 txt_file = 'input_1a.txt'
 puzzle_input = Puzzle_Input(txt_file)
-print(puzzle_input.read())
+results = []
+for i in range(1, len(puzzle_input.list)):
+    previous = puzzle_input.list[i-1]
+    current = puzzle_input.list[i]
+    result = Comparer(current, previous).a_lgt_b()
+    results.append(result)
+    #print(f'{current}  {previous}  {result}')
+print(len(puzzle_input.list))
+print(len(results))
+print(sum(results)) # the correct result was 1451 - instead of 1450 (I do not understand the reason)
