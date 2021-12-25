7afdb0fc57daa538dfe7dff5d47a3429623111fd
Oliver <omoell@gmail.com>
2021-12-25 21:49:11 +0100

    added solutions for 2a and 2b

--- 2021/1.py
+++ 2021/1.py
@@ -12,14 +12,47 @@ class Puzzle_Input:
 
     def drop_speed(self, rolling=1):
         if self.type == 'int' and rolling > 0:
-            data = puzzle_input.list if rolling == 1 else [sum(x) for x in zip(puzzle_input.list, puzzle_input.list[1:], puzzle_input.list[2:])]
+            data = self.list if rolling == 1 else [sum(x) for x in zip(self.list, self.list[1:], self.list[2:])]
         else: sys.exit(f'type of data has to be "numbers" and "rolling" must be a positive value.')
 
         return sum(y > x for x, y in zip(data, data[1:])) # this one line solution was copied from reddit thread - author not known anymore
 
+    def steps(self):
+        steps_raw = [step.split(' ') for step in self.list]
+        return [[direction, (-int(value) if direction == 'up' else int(value))] for direction, value in steps_raw]
 
-# Puzzle 1a
-txt_file = 'input_1a.txt'
-puzzle_input = Puzzle_Input(txt_file, 'int')
-print(f'Solution for 1a: {puzzle_input.drop_speed()}')
-print(f'Solution for 1b: {puzzle_input.drop_speed(3)}')
+    def final_position(self):
+        horizontal = 0
+        depth = 0
+        steps = self.steps()
+        for direction, value in steps:
+            if direction == 'forward':
+                horizontal = horizontal + value
+            else:
+                depth = depth + value
+        return horizontal * depth
+
+    def final_position_w_aim(self):
+        horizontal = 0
+        aim = 0
+        depth = 0
+        steps = self.steps()
+        for direction, value in steps:
+            if direction == 'forward':
+                depth = depth + (aim * value)
+                horizontal = horizontal + value
+            else:
+                aim = aim + value
+        return horizontal * depth
+
+# Puzzle 1
+puzzle1_file = 'input_1.txt'
+puzzle1 = Puzzle_Input(puzzle1_file, 'int')
+print(f'Solution for 1a: {puzzle1.drop_speed()}')
+print(f'Solution for 1b: {puzzle1.drop_speed(3)}')
+
+# Puzzle 2
+puzzle2_file = 'input_2.txt'
+puzzle_2 = Puzzle_Input(puzzle2_file, 'string')
+print(f'Solution for 2a: {puzzle_2.final_position()}')
+print(f'Solution for 2b: {puzzle_2.final_position_w_aim()}')
